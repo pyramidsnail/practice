41bc2410ecacfb986d5385e69f327f464347afee
pyramidsnail <sherry.bim@gmail.com>
2014-05-23 09:18:03 +0200

    finally got the right answer :@)

--- p1.py
+++ p1.py
@@ -1,9 +1,36 @@
 import os, sys
 
+def sortCount(a,b):
+    c=[]
+    global inv
+    i, j = 0, 0
+    while i<len(a) and j<len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            inv += len(a)-i
+            c.append(b[j])
+            j += 1
+    if i == len(a):
+        c.extend(b[j:])
+    else:
+        c.extend(a[i:])
+    return c
+def divide(d):
+    if len(d)>1:
+        a = divide(d[0:int(len(d)/2)])
+        b = divide(d[int(len(d)/2): len(d)])
+        return sortCount(a,b)
+    else:
+        return d
 
-r=0
-def sortCount(a):
-    if len(a) == 1:
-        return r
-    
-        
+
+inv = 0
+array=[]
+f=open('../IntegerArray.txt','r')
+for i in f.readlines():
+    array.append(int(i.strip()))
+# array = [5,4,3,2,1]
+sorted = divide(array)
+print inv
