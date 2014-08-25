db45475eb22549c1fc0a7f7af1c6cf98721375f4
Lin Wang <wangli@tbi-ds17.inet.dkfz-heidelberg.de>
2014-08-25 18:00:04 +0200

    345

--- spoj/345.py
+++ spoj/345.py
@@ -1,5 +1,25 @@
 n = int(raw_input())
 colors = [int(x) for x in raw_input().split()]
+dp = [[float("inf") for x in xrange(100)] for x in xrange(100)]
+def mix(a,b):
+    total = 0
+    for i in xrange(a-1,b):
+        total += colors[i]
+    return total%100
+
+def solve(a,b):
+    if a==b:
+        dp[a][b]=0
+    elif a+1==b:
+        dp[a][b] = colors[a-1]*colors[b-1]
+    else:
+        for i in xrange(a,b+1):
+            dp[a][b]=min(dp[a][b], dp[a][i]+dp[i+1][b]+mix(a,i)*mix(i+1,b))
+    return dp[a][b]
+
+print solve(1,n)
+
+
 
 
 
